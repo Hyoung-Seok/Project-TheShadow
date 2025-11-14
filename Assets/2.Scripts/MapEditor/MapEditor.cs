882deae3d739361bd38c6ab9cffd4854da86bed5
Hyoung-Seok <dkavmdk98@gmail.com>
2025-11-14 18:38:57 +0900

    [MapEditor] 그리드 생성 시스템 구현

--- Assets/2.Scripts/MapEditor/MapEditor.cs
+++ Assets/2.Scripts/MapEditor/MapEditor.cs
@@ -1,7 +1,39 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MapEditor : MonoBehaviour
 {
+    [Header("Grid Setting")]
     [SerializeField] private GameObject gridTile;
-    [SerializeField] private Vector2 gridSize;
+    [SerializeField] private Transform gridParent;
+    [SerializeField] private Vector2Int gridSize;
+    [SerializeField] private float cellSize = 1f;
+
+    private void Start()
+    {
+        CreateGridTile();
+    }
+
+    private void CreateGridTile()
+    {
+        var center = gridParent.position;
+
+        var startX = center.x - (gridSize.x * cellSize) / 2 + cellSize * 0.5f;
+        var startZ = center.z + (gridSize.y * cellSize) / 2 + cellSize * 0.5f;
+        var pos = new Vector3();
+        
+        for (var i = 0; i < gridSize.x; ++i)
+        {
+            for (var j = 0; j < gridSize.y; ++j)
+            {
+                pos.x = startX + j * cellSize;
+                pos.y = center.y;
+                pos.z = startZ - i * cellSize;
+
+                Instantiate(gridTile, pos, Quaternion.identity, gridParent);
+            }
+        }
+
+    }
 }
