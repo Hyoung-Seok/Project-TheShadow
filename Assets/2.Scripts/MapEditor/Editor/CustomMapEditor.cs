1f0c6158c6345100d453d0b81e4e6dd9d1c8d267
Hyoung-Seok <dkavmdk98@gmail.com>
2025-11-14 17:48:34 +0900

    [MapEditor] 타일 프리팹 생성

--- Assets/2.Scripts/MapEditor/Editor/CustomMapEditor.cs
+++ Assets/2.Scripts/MapEditor/Editor/CustomMapEditor.cs
@@ -1,18 +1,18 @@
-using UnityEngine;
-using UnityEditor;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
-
-[CustomEditor(typeof(MapEditor))]
-public class CustomMapEditor : Editor
-{
-    public VisualTreeAsset VisualTreeAsset;
-    
-    public override VisualElement CreateInspectorGUI()
-    {
-        var root = new VisualElement();
-        VisualTreeAsset.CloneTree(root);
-
-        return root;
-    }
-}
+// using UnityEngine;
+// using UnityEditor;
+// using UnityEditor.UIElements;
+// using UnityEngine.UIElements;
+//
+// [CustomEditor(typeof(MapEditor))]
+// public class CustomMapEditor : Editor
+// {
+//     public VisualTreeAsset VisualTreeAsset;
+//     
+//     public override VisualElement CreateInspectorGUI()
+//     {
+//         var root = new VisualElement();
+//         VisualTreeAsset.CloneTree(root);
+//
+//         return root;
+//     }
+// }
